2e28a5b8f0a57ccd995a4882f9a831ee4d6a2122
nxnaxx <nxnaxx125@gmail.com>
2024-09-23 09:46:48 +0900

    chore: Initialize project with basic setup

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# cs-quiz
+# CS Quiz Hub
+
+CS 지식을 퀴즈 형태로 학습하는 사이트
---
+++ eslint.config.js
@@ -0,0 +1,35 @@
+import { defineConfig } from 'eslint-define-config';
+
+export default defineConfig({
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier', '@emotion'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
+    'plugin:@emotion/recommended',
+  ],
+  rules: {
+    'prettier/prettier': ['error'],
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'no-console': 'warn',
+  },
+  ignorePatterns: ['node_modules', 'build'],
+});
