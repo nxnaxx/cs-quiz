bd0d98d08d9dc7e197125eb4c8462350d214e59c
nxnaxx <nxnaxx125@gmail.com>
2024-10-02 15:24:37 +0900

    feat: Add responsive design to topics page

--- src/pages/Topics.tsx
+++ src/pages/Topics.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import styled from '@emotion/styled';
+import { mobile, tabletL, tabletS } from '@styles/responsive';
 import { topics } from '@data/topics';
 import MainTemplate from '@templates/MainTemplate';
 import TopicCard from '@molecules/card/TopicCard';
@@ -11,12 +12,31 @@ import { Topic } from 'src/types/quizTypes';
 const TopicTitle = styled.h2`
   margin-bottom: 48px;
   text-align: center;
+
+  @media (max-width: ${mobile}) {
+    margin-bottom: 40px;
+  }
 `;
 
 const TopicList = styled.div`
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   gap: 32px;
+
+  @media (max-width: ${tabletL}) {
+    grid-template-columns: repeat(3, 1fr);
+    padding: 0 96px;
+  }
+
+  @media (max-width: ${tabletS}) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media (max-width: ${mobile}) {
+    grid-template-columns: repeat(1, 1fr);
+    gap: 24px;
+    padding: 0 24px;
+  }
 `;
 
 export default function Topics() {
