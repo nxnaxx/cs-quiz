2b179eb035a8f7e58254b25d9876ecf1a5fa4665
nxnaxx <nxnaxx125@gmail.com>
2024-10-04 17:44:31 +0900

    fix: Prevent API call on prev page navigation during loading

--- src/services/openAIService.ts
+++ src/services/openAIService.ts
@@ -1,35 +1,51 @@
-import OpenAI from 'openai';
 import { OptionStore } from '@store/useOptionStore';
 import { cleanResponse, createQuizPrompt, quizTypeInterface } from '@utils/quizUtils';
 
-const openai = new OpenAI({
-  apiKey: import.meta.env.VITE_OPENAI_API_KEY,
-  dangerouslyAllowBrowser: true,
-});
-
-const fetchQuiz = async ({ topic, difficulty, quizNum, quizType }: OptionStore['optionValues']) => {
+const fetchQuiz = async (
+  { topic, difficulty, quizNum, quizType }: OptionStore['optionValues'],
+  signal?: AbortSignal,
+) => {
   const quizTopic = (topic === '프레임워크 및 라이브러리' ? '리액트, SSR과 CSR' : topic) || '';
   const promptInterface = quizTypeInterface(quizType);
   const quizPrompt = createQuizPrompt(quizTopic, quizType, quizNum, difficulty, promptInterface);
 
-  const completion = await openai.chat.completions.create({
-    messages: [
-      {
-        role: 'system',
-        content:
-          "You are a extremely accurate and diligent quiz generator specializing in high-quality computer science quizzes. Your only role is to create precise, well-structured quizzes based on the user's provided topics and difficulty levels.",
+  try {
+    const response = await fetch('https://api.openai.com/v1/chat/completions', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${import.meta.env.VITE_OPENAI_API_KEY}`,
       },
-      {
-        role: 'user',
-        content: quizPrompt,
-      },
-    ],
-    model: 'gpt-3.5-turbo',
-  });
+      body: JSON.stringify({
+        model: 'gpt-3.5-turbo',
+        messages: [
+          {
+            role: 'system',
+            content:
+              "You are a extremely accurate and diligent quiz generator specializing in high-quality computer science quizzes. Your only role is to create precise, well-structured quizzes based on the user's provided topics and difficulty levels.",
+          },
+          {
+            role: 'user',
+            content: quizPrompt,
+          },
+        ],
+      }),
+      signal,
+    });
 
-  const rawResponse = completion.choices[0].message.content!;
-  const response = cleanResponse(rawResponse);
-  return JSON.parse(response);
+    if (!response.ok) throw new Error(`API 호출 실패: ${response.statusText}`);
+
+    const completion = await response.json();
+    const rawResponse = completion.choices[0].message.content;
+    const cleanedResponse = cleanResponse(rawResponse);
+    return JSON.parse(cleanedResponse);
+  } catch (error) {
+    if (error instanceof Error && error.name === 'AbortError') {
+      console.log('API 호출이 중단되었습니다.');
+    } else console.error('알 수 없는 오류 발생:', error);
+
+    throw error;
+  }
 };
 
 export default fetchQuiz;
